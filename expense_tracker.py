801b55a38d94dc09a3fcc2e89dedfa08727b11fb
TNorth1 <89692406+TNorth1@users.noreply.github.com>
2024-12-25 13:28:16 +0000

    Altered JSON structure to follow best practices

--- expense_tracker.py
+++ expense_tracker.py
@@ -5,7 +5,13 @@ import os
 def get_storage_directory():
 
     config_path = "config.json"
-    default_config_data = {"file_storage_directory": "NOT_SET"}
+    # TODO make this more universal so it doesn't re write all config...
+    # once more settings are added
+    default_config_data = {
+        "storageDirectory": {
+            "directory": "NOT_SET"
+        }
+    }
 
     try:
         with open(config_path, "r") as config_file:
@@ -17,13 +23,13 @@ def get_storage_directory():
         config = default_config_data
 
     # if the storage directory is not set, make the user set the directory
-    if (config.get("file_storage_directory") == "NOT_SET" or
-            # Checks that user has not edited the config to invalid values
-            not os.path.isdir(config.get("file_storage_directory"))):
+    if (config["storageDirectory"].get("directory") == "NOT_SET" or
+            # Checks that user has not edited the config to directory that doesn't exits
+            not os.path.isdir(config["storageDirectory"].get("directory"))):
         while True:
             directory = input("Set the directory to store expense reports: ")
             if os.path.isdir(directory):
-                config["file_storage_directory"] = directory
+                config["storageDirectory"]["directory"] = directory
                 with open(config_path, "w") as config_file:
                     json.dump(config, config_file, indent=4)
                 break
@@ -31,4 +37,4 @@ def get_storage_directory():
                 print(
                     "Directory does not exist, please try again or create the directory")
 
-    return config["file_storage_directory"]
+    return config["storageDirectory"]["directory"]

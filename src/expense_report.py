0e537c2d8f823735f3b8a784940999d31a32ddc0
TNorth1 <89692406+TNorth1@users.noreply.github.com>
2025-01-15 20:12:58 +0000

    - Added functionality to create a grand total row             - added calculate_grand_total_row method             - added create_grand_total_row method             - added combine_data_with_grand_totals
        - Created methods to display the table in the terminal
                - added populate_table method
                - added print_table method
    
        - TODO
                - get rid of combine_data_with_grand_totals for better reusability
                - split up populate table to create a table method and one to populate the table
                - create a method that prints out the final row seperately for better reusability

--- src/expense_report.py
+++ src/expense_report.py
@@ -59,30 +59,87 @@ class ExpenseReport:
         ExpenseReport.save_expense_report(report_file, report_path)
 
     @staticmethod
-    def format_report_rows(report_data):
-        """Formats the report rows to be viewed correctly in the terminal. e.g. 9.0 -> £9"""
+    def calculate_grand_total_row(report_data):
+        """Calculates the grand total of expenses in a expense report"""
+
+        grand_total = 0
+        claimable_grand_total = 0
+
         for row in report_data:
+            grand_total += row["Total"]
+            claimable_grand_total += row["Claimable Total"]
+
+        return grand_total, claimable_grand_total
+
+    @staticmethod
+    def create_grand_total_row(grand_total_pair):
+        """Create a row for the grand total and the claimable grand total"""
+        # unpack the grand_total_pair tuple
+        grand_total, claimable_grand_total = grand_total_pair
+
+        # the Grand Total replaces the Total
+        # The Claimable Grand Total replaces the Claimable Total
+        grand_total_row = {
+            "Date": "",
+            "Breakfast": "",
+            "Lunch": "",
+            "Dinner": "",
+            "Total": grand_total,
+            "Claimable Total": claimable_grand_total
+        }
+
+        return grand_total_row
+
+    @staticmethod
+    def combine_data_with_grand_totals(report_data, grand_total_row):
+        """
+        Combines report data with a grand totals row. Does not alter the report file,
+        it is just for the purposes of displaying the report table
+        """
+        # Convert grand_total_row to a list, to allow concatenation with report data list
+        return report_data + [grand_total_row]
+
+    @staticmethod
+    def format_report_data(report_data_with_totals):
+        """Formats the report rows to be viewed correctly in the terminal. e.g. 9.0 -> £9"""
+        for row in report_data_with_totals:
             for key, value in row.items():
                 if isinstance(value, (int, float)):
-                    # if value float is a digit e.g. 10.0. value -> £10
+                    # if value float is a digit e.g. 10.0 value -> £10
                     # else value float e.g. 10.01 -> £10.01
                     row[key] = f"£{int(value) if value ==
                                    int(value) else value}"
 
-        formatted_report_row = report_data
-        return formatted_report_row
+        formatted_report_data = report_data_with_totals
+
+        return formatted_report_data
 
     @staticmethod
-    def view_report(report_name, formatted_report_data):
+    def populate_table(report_name, formatted_report_data):
+        # TODO add a create/init table method that splits this down
+        # Make this more reusable by making adding the total row
+        # a separate function
         """Display the contents of the report in the terminal"""
         table = Table(title=report_name,
-                      header_style="bold blue", border_style="bold green")
+                      header_style="bold magenta", border_style="bold green")
 
         for column in formatted_report_data[0].keys():
             table.add_column(column)
 
-        for item in formatted_report_data:
-            table.add_row(*[str(value) for value in item.values()])
+        for item in formatted_report_data[:-1]:
+            table.add_row(*[value for value in item.values()])
+            # Add a line between each row
+            table.add_section()
 
+        # Add extra line after report rows
+        table.add_section()
+        # Add the grand total row to the table
+        table.add_row(*[value for value in formatted_report_data[-1].values()])
+
+        return table
+
+    @staticmethod
+    def print_table(table):
+        "Prints the formatted table"
         console = Console()
         console.print(table)

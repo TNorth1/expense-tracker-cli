6e9ea743564409ecaad335a30a87df413b58253a
TNorth1 <89692406+TNorth1@users.noreply.github.com>
2025-03-21 00:05:20 +0000

    Updated setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="expense-tracker-cli",
+    version="0.1.0",
+    author="TNorth1",
+    description="A command-line expense tracker",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/TNorth1/expense-tracker-cli",
+    packages=find_packages(),
+    install_requires=[
+        "pandas>=2.2.3",
+        "rich>=13.9.4",
+        "platformdirs>=4.3.6",
+        "XlsxWriter>=3.2.0"
+    ],
+    entry_points={
+        "console_scripts": ["exptrack=src.main:main"],
+    },
+    python_requires=">=3.9",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    keywords="expense tracker CLI",
+    include_package_data=True,
+)

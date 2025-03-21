6b5d93abfcd0574a0c6a38d0fd5c36d1f6243a11
TNorth1 <89692406+TNorth1@users.noreply.github.com>
2025-03-21 15:03:44 +0000

    Refactored add_new_report_entry function, to clean up loop logic

--- src/commands.py
+++ src/commands.py
@@ -58,11 +58,11 @@ def display_report(
 
 def add_new_report_entry(report_path: str) -> None:
     """Add a new expense to report and ask user to add another expense"""
-    continue_adding_expense = True
-    while continue_adding_expense:
+    while True:
         expense = user_input.get_report_data()
         utils.add_expense_to_report(expense, report_path)
-        continue_adding_expense = user_input.continue_adding_expenses()
+        if not user_input.continue_adding_expenses():
+            break
 
 
 def list_reports(storage_directory: str, console: Console) -> None:
